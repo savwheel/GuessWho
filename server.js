dc81d74f99ecaf63fb9eda48be2a48bf8c7164c7
Srobison12 <srobison@nmu.edu>
2018-11-26 09:37:47 -0500

    Updated Server for Mongo and Socket

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+var client = new MongoClient("mongodb://localhost:27017", {useNewUrlParser: true});
+var db;
+
 var express = require("express");
+
 var server = express();
+
+var http = require("http");
+
+var server = http.Server(app);
+var socketio = require("socket.io");
+var io = socketio(server);
+
 server.use(express.static("./pub"));
 var bodyParser = require("body-parser");
 server.use(bodyParser.urlencoded({extended: true}));
+
+
+io.on("connection", function(socket) {
+    console.log("A user connected");
+
+    //below will be used for socket stuff on server side
+});
 
 server.listen(80, function() {
     console.log("Server waiting on port 80 . . .")
