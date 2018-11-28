6f051ea5bc9a71cf210bc6b4f66bef53e81ed0a8
Savannah Wheeler <savwheel@nmu.edu>
2018-11-27 21:12:35 -0500

    I moved the dialog box

--- pub/client.js
+++ pub/client.js
@@ -19,7 +19,9 @@ function populate() {
 	var table = document.getElementById("board");
 
 	var thePlayer = document.getElementById("yourPlayer");
-	$(thePlayer).append('<div class="col mini-box"><img id="playerImg" src="img/charlie.jpg" alt="Photo of blank identity"></div>')
+	$(thePlayer).prepend('<div class="col mini-box"><img id="playerImg" src="img/charlie.jpg" alt="Photo of blank identity"></div>')
+	$(thePlayer).prepend("<h2>Your Character</h2>");
+
 	for(var i = 0; i < 3; i++) {
 		$(table).append('<div class="row" id ="row' + i + '">');
 		for(var j = 0; j < 6; j++) {
@@ -30,6 +32,15 @@ function populate() {
 		}
 
 	}
+
+
+
+	$("#thename").click(function() {
+		if($(this).css('opacity')==0.2){
+			$(this).css('opacity','1.0');
+		}
+		else $(this).css('opacity','0.2');
+	});
 }
 
 //If the window is smaller than a certain size then the board moves down and the chat and leader board resize
@@ -48,21 +59,9 @@ function startThings() {
 	$("#startScreen").show();
 
 	var table = document.getElementById("board");
-	// if (!$("#board td").html()) {
-	// 	populate();
-	// }
 
-
-	populate();
 	changeSizeDispaly();
-	//This part doesn't work yet
-	// for(var i = 0; i < 4; i++) {
-	// 	for(var j = 0; j < 6; j++) {
-	// 		table.rows[i].cells[j].onclick = function() {
-	// 			$(table.rows[i].cells[j]).css("opacity", ".7");		//When they click a photo it resets the opacity so they can see its been eliminated
-	// 		}
-	// 	}
-	//}
+
 
 	$("#submit").click(function() {
 		socket.emit("addUser", $("#username").val(), function(loginSuccessful) {
@@ -76,12 +75,6 @@ function startThings() {
 
 
 
-	$("#thename").click(function() {
-		if($(this).css('opacity')==0.2){
-			$(this).css('opacity','1.0');
-		}
-		else $(this).css('opacity','0.2');
-	});
 
 	//when they send a message to the chat, call back, clear msg
 	$("#chatButton").click(function(){
