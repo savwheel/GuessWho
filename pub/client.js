595acfa73f4b0fce7a2093b23a50636d3f4c389b
Srobison12 <srobison@nmu.edu>
2018-11-30 09:29:56 -0500

    Leaderboard

--- pub/client.js
+++ pub/client.js
@@ -5,10 +5,10 @@ var myUsername = null;
 //need to change formatting and test once query is working.
 socket.on("updateClientLeaderBoard", function(scoreArray){
 	$("leaderBoardScore").val("");
-	var score;
-	for(score in scoreArray){
-		console.log(scoreArray[score]);
-		$("leaderBoardScore").append(scoreArray[score]+"<br>");
+	var s;
+	for(s in scoreArray){
+		console.log(scoreArray[s].name);
+		$("leaderBoardScore").append(scoreArray[s]+"<br>");
 	}
 });
 
--- server.js
+++ server.js
@@ -69,7 +69,7 @@ io.on("connection", function(socket) {
 
     //getLeaderboard will query the DB, and return an array of the scores
     socket.on("getLeaderboard", function(){
-        db.collection("scores").find({}).toArray(function(err, docs){
+        db.collection("scores").find({},{projection:{'_id':0}}).toArray(function(err, docs){
             if(err!=null){
                 console.log("Error: " + err);
             }else{
